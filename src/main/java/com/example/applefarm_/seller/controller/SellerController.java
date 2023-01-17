fa0cb3e04dde569fd0e7bbdc7425086ca2983c4d
dangddoong <dangddoong@gmail.com>
2023-01-17 18:30:51 +0900

    3-4 [feat] 판매자 프로필 관련 업데이트

--- src/main/java/com/example/applefarm_/seller/controller/SellerController.java
+++ src/main/java/com/example/applefarm_/seller/controller/SellerController.java
@@ -1,9 +1,32 @@
 package com.example.applefarm_.seller.controller;
 
+import com.example.applefarm_.security.user.UserDetailsImpl;
+import com.example.applefarm_.seller.dto.SellerProfileResponseDto;
+import com.example.applefarm_.seller.dto.UpdateSellerProfileDto;
+import com.example.applefarm_.seller.service.SellerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class SellerController {
+    private final SellerService sellerService;
+    //seller 프로필 수정
+    @GetMapping("/sellers/profile")
+    public SellerProfileResponseDto getMySellerProfile(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return new SellerProfileResponseDto(userDetails.getUser());
+    }
+    //seller 프로필 조회
+    @PutMapping("/sellers/profile")
+    public void updateMySellerProfile(@RequestBody @Valid UpdateSellerProfileDto updateSellerProfileDto,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails){
+        sellerService.updateMySellerProfile(updateSellerProfileDto, userDetails.getUsername());
+    }
 }
+
---
+++ src/main/java/com/example/applefarm_/seller/dto/UpdateSellerProfileDto.java
@@ -0,0 +1,21 @@
+package com.example.applefarm_.seller.dto;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@RequiredArgsConstructor
+public class UpdateSellerProfileDto {
+    @NotNull(message = "닉네임에는 공백이 들어갈 수 없습니다.")
+    private final String sellerNickname;
+    @NotNull(message = "이미지에는 공백이 들어갈 수 없습니다.")
+    private final String sellerImage;
+    @NotNull(message = "소개글에는 공백이 들어갈 수 없습니다.")
+    private final String sellerDetails;
+    @NotNull(message = "매칭주제정보에는 공백이 들어갈 수 없습니다.")
+    private final String sellerCategory;
+
+}
