4444a9e8e2d9ec96b3ce11cbbabb9760ba046d14
milnil0228 <milnil0228@gmail.com>
2023-01-18 11:29:50 +0900

    4-4 [fix] 어드민에서 셀러 등록 허용 시 등록 요청 내용 반영

--- src/main/java/com/example/applefarm_/registration/entity/Registration.java
+++ src/main/java/com/example/applefarm_/registration/entity/Registration.java
@@ -1,13 +1,17 @@
 package com.example.applefarm_.registration.entity;
 
 import com.example.applefarm_.registration.dto.RegistrationRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
 @Entity
+@NoArgsConstructor
 public class Registration {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
